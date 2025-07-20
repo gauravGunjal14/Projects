bf723a582d3960c8f3ca1a94be7df9211be89008
gaurav-gunjal14 <gauravgunjal45@gmail.com>
2025-07-20 13:04:11 +0530

    added menu bar for mobile view

--- DSA-Visulizer/app.js
+++ DSA-Visulizer/app.js
@@ -1,20 +1,48 @@
+// Dark Mode and Light Mode
 const toggelBtn = document.getElementById("toggelBtn");
+const themeIcon = document.getElementById("themeIcon")
 const body = document.querySelector("body");
 
 if (localStorage.getItem("theme") === "light") {
     body.classList.add("light-mode");
-    toggelBtn.textContent = "🌙";
+    themeIcon.src = "../icons/dark-mode.png";
 }
 
 toggelBtn.addEventListener("click", () => {
     body.classList.toggle("light-mode");
 
     if (body.classList.contains("light-mode")) {
-        toggelBtn.textContent = "🌙";
+        themeIcon.src = "../icons/dark-mode.png";
         localStorage.setItem("theme", "light");
     }
     else {
-        toggelBtn.textContent = "🌞";
+        themeIcon.src = "../icons/light-mode.png";
         localStorage.setItem("theme", "dark");
     }
+});
+
+// menuIcon logic
+const menuIcon = document.getElementById("menuIcon");
+const closeIcon = document.getElementById("closeIcon");
+const navLinks = document.querySelector(".nav-links");
+
+menuIcon.addEventListener("click", () => {
+    navLinks.classList.add("show");
+    menuIcon.style.display = "none";
+    closeIcon.style.display = "block";
+});
+
+closeIcon.addEventListener("click", () => {
+    navLinks.classList.remove("show");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+});
+
+const allLinks = document.querySelectorAll(".nav-links a");
+allLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("show");
+        closeIcon.style.display = "none";
+        menuIcon.style.display = "block";
+    });
 });
